93732b9f1ce98665689cfa00fa0d8ea3470c4eba
Walaryne <hazardoustheradioactive@gmail.com>
2020-05-29 01:48:43 -0400

    First working prototype

--- src/main/java/com/walaryne/flymod/FlyCommand.java
+++ src/main/java/com/walaryne/flymod/FlyCommand.java
@@ -1,18 +1,37 @@
 package com.walaryne.flymod;
 
 import com.mojang.brigadier.CommandDispatcher;
+import com.mojang.brigadier.context.CommandContext;
+import com.mojang.brigadier.exceptions.CommandSyntaxException;
 import com.mojang.brigadier.tree.LiteralCommandNode;
-import net.minecraft.server.command.CommandSource;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.server.command.ServerCommandSource;
 
-import static com.mojang.brigadier.arguments.StringArgumentType.getString; // getString(ctx, "string")
-import static com.mojang.brigadier.arguments.StringArgumentType.word; // word(), string(), greedyString()
-import static net.minecraft.server.command.CommandManager.argument; // argument("bar", word())
-import static net.minecraft.server.command.CommandManager.*; // Import everything
+import static net.minecraft.server.command.CommandManager.literal;
 
-import static com.mojang.brigadier.builder.LiteralArgumentBuilder.literal;
 
 public class FlyCommand {
-    public static LiteralCommandNode register(CommandDispatcher<CommandSource> command) {
-        return dispatcher.register(literal("fly"));
+    public static LiteralCommandNode<ServerCommandSource> register(CommandDispatcher<ServerCommandSource> dispatcher, boolean dedicated) {
+        return dispatcher.register(literal("fly")
+                .executes(FlyCommand::fly));
+    }
+
+    private static int fly(CommandContext<ServerCommandSource> ctx) throws CommandSyntaxException {
+        final ServerCommandSource source = ctx.getSource();
+
+        final PlayerEntity player = source.getPlayer();
+
+        if (!player.abilities.allowFlying) {
+            player.abilities.allowFlying = true;
+            //toggleString = "commands.fly.successfulon";
+        } else {
+            player.abilities.allowFlying = false;
+            player.abilities.flying = false;
+            //toggleString = "commands.fly.successfuloff";
+        }
+
+        player.sendAbilitiesUpdate();
+
+        return 1;
     }
 }
--- src/main/java/com/walaryne/flymod/FlyModInitialization.java
+++ src/main/java/com/walaryne/flymod/FlyModInitialization.java
@@ -1,13 +1,13 @@
 package com.walaryne.flymod;
 
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.registry.CommandRegistry;
+import net.fabricmc.fabric.api.command.v1.CommandRegistrationCallback;
 
 
 public class FlyModInitialization implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-		//CommandRegistrationCallback.EVENT.register(FlyCommand::register);
+		CommandRegistrationCallback.EVENT.register(FlyCommand::register);
 	}
 }
